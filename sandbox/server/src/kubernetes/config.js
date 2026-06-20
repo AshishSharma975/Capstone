c8ef7e306ce7ab0f0ae02d529e81fef73d053497
AshishSharma975 <ashish8389sharma@gmail.com>
2026-06-20 09:43:03 +0530

    feat: initialize Kubernetes client configuration and export CoreV1Api instance

--- sandbox/server/src/kubernetes/config.js
+++ sandbox/server/src/kubernetes/config.js
@@ -3,4 +3,6 @@ import * as k8sApi from "@kubernetes/client-node";
 const kc = new k8sApi.KubeConfig();
 kc.loadFromDefault();
 
+console.log("Current Cluster:", kc.getCurrentCluster());
+
 export const k8sCoreV1Api = kc.makeApiClient(k8sApi.CoreV1Api);
