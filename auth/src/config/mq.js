939be116d9cce333dbc4f27d44be29c8eb27077a
AshishSharma975 <ashish8389sharma@gmail.com>
2026-07-11 20:02:44 +0530

    feat: add auth service deployment, RabbitMQ message producer, and email notification microservice

---
+++ auth/src/config/mq.js
@@ -0,0 +1,13 @@
+import amqplib from "amqplib";
+
+
+const QUEUE = 'auth_notification_queue';
+
+const connection = await amqplib.connect(process.env.RABBITMQ_URL);
+const channel = await connection.createChannel();
+
+channel.assertQueue(QUEUE, {durable: true});
+
+function publishToQueue(queue, content){
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(content)), {persistent: true});
+}
