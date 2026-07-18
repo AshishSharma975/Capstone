ad092c960204e3fe48e05b7aa397d706c4fd6cdc
AshishSharma975 <ashish8389sharma@gmail.com>
2026-07-18 16:17:51 +0530

    feat: initialize express application with Google OAuth2 authentication and health check endpoints

--- auth/src/app.js
+++ auth/src/app.js
@@ -29,7 +29,7 @@ app.get("/_status/healthz",(req,res)=>{
     res.status(200).send("OK...")
 })
 app.get("/_status/readyz",(req,res)=>{
-    res.status(200).send("OK")
+    res.status(200).send("OK...")
 })
 
 app.use("/api/auth",authRoutes)
