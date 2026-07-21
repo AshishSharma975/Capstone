299bc46fa9f40a03763a3666947ae70c356cae3b
AshishSharma975 <ashish8389sharma@gmail.com>
2026-07-21 17:44:50 +0530

    feat: initialize server project with Mongoose, MongoDB dependencies and basic application structure

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -6,13 +6,15 @@ import { createService } from "./kubernetes/service.js";
 import { cleanupOldSandboxes } from "./kubernetes/cleanup.js";
 import {v7 as uuid} from "uuid"
 import { createSandboxKey } from "./config/redis.js";
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 
 app.get("/api/sandbox/health", (req, res) => {
     res.status(200).json({
